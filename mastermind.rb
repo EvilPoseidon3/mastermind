e80c88d57b7820ba0710b261221d6aeb9dfb508b
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-05-22 11:29:11 -0500

    got board class set up asking question on which version of game to be played

--- mastermind.rb
+++ mastermind.rb
@@ -6,18 +6,18 @@ module ChangeAlgo
 
 end
 
-class Game
+class Game < Board
 
   def initialize(player)
-
+    super
   end
 
 end
 
 class Board 
- attr_accessor :row1,:row2, :column1,:column2,:column3,:column4,:column5,:column6,:column7,:column8,:column9,:column10,:column11,:column12
+ attr_accessor :board,:row1,:row2, :column1,:column2,:column3,:column4,:column5,:column6,:column7,:column8,:column9,:column10,:column11,:column12
   def initialize
-   @title = "   MASTERMIND   "
+   @title ="   MASTERMIND    "
    @row1 = "-----------------"
    @row2 = "--^---^---^---^--"
    @column1 = "|   |   |   |   |"   
@@ -32,8 +32,21 @@ class Board
    @column10 = "|   |   |   |   |"   
    @column11 = "|   |   |   |   |"   
    @column12 = "|   |   |   |   |"  
-  puts @board = [@row1, @title, @row1, @column1, @row2, @column2, @row1, @column3, @row1, @column4, @row1, @column5, @row1, @column6, @row1, @column7, @row1, @column8, @row1, @column9, @row1, @column10, @row1, @column11, @row1, @column12, @row1,]
-  end
+   @board = [@row1, @title, @row1, @column1, @row2, @column2, @row1, @column3, @row1, @column4, @row1, @column5, @row1, @column6, @row1, @column7, @row1, @column8, @row1, @column9, @row1, @column10, @row1, @column11, @row1, @column12, @row1,]
+   @display_board = @board.map { |element| "           " + element.to_s}
+   puts @display_board
+   puts "   Would you like to play against the "
+   puts" computer or try and beat the computer?"
+   puts "(Yes) For against the computer"
+   puts "(No) To face the computer."
+   @ats = gets.chomp.downcase
+   if @ats == "yes"
+    # run normal game
+   elsif
+    # run challenge game
+   else
+    puts "not a valid choice try again."
+end 
 
 end
 
